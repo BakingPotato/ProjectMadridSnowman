3befadd8ec812c6f0365530108e9eb25f7164117
Pistoncito <victorglezrivera@hotmail.com>
2022-11-12 14:50:15 +0100

    shooter controller (los disparos se desvian)

--- Assets/Scripts/ShooterController.cs
+++ Assets/Scripts/ShooterController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,27 +11,92 @@ public class ShooterController : MonoBehaviour
     [Range(1.0f, 10.0f)]
     public float reloadTime;
 
-    [Tooltip("Instance of the player to get his/her rotation")]
-    public GameObject playerInstance;
+    [Range(0.5f, 5.0f)]
+    public float proyectileTimeAlive;
+
+    [Tooltip("Instance of the owner")]
+    public GameObject owner;
 
     [Tooltip("Snow model prefab")]
     public GameObject snowballPrefab;
 
-
+    [Range(1, 10)]
+    public int poolSize;
+    private Queue<GameObject> pool;
 
     void Start()
     {
-        
+        InitPool();
+        StartCoroutine(StartInputDetection());
     }
 
-
-    void Update()
+    private void InitPool()
     {
-        
+        pool = new Queue<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject instance = Instantiate(snowballPrefab);
+            instance.SetActive(false);
+            pool.Enqueue(instance);
+        }
     }
 
-    public bool Launch()
+    private IEnumerator StartInputDetection()
     {
-        return false;
+        while(true)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                LaunchProyectile();
+            }
+            yield return null;
+        }
+  
     }
+
+
+    public bool LaunchProyectile()
+    {
+        try
+        {
+            GameObject proyectile = pool.Dequeue();
+            proyectile.transform.rotation = Quaternion.identity;
+            proyectile.SetActive(true);
+            float eulerYRot = owner.transform.localRotation.eulerAngles.y;
+           
+            //if (eulerYRot < 0.0f) eulerYRot += 360.0f;
+            Debug.Log("local euler" + owner.transform.localRotation.eulerAngles);
+            Vector3 directorVector = new Vector3(Mathf.Cos(eulerYRot), 0, Mathf.Sin(eulerYRot));
+            //position
+            //Vector3 boundsSize = proyectile.GetComponent<Collider>().bounds.size;
+            //Vector3 positionSpawnOffset = directorVector + new Vector3(boundsSize.x, boundsSize.y, boundsSize.z);
+            //proyectile.transform.position = owner.transform.position + positionSpawnOffset;
+            //Direction
+            proyectile.GetComponent<Rigidbody>().velocity = directorVector.normalized * shotSpeed;
+
+
+            StartCoroutine(AliveProyectile(proyectile));
+            return true;
+
+        }catch(InvalidOperationException e)
+        {
+            Debug.Log("there is no proyectile left");
+            return false;
+        }
+    }
+
+    private IEnumerator AliveProyectile(GameObject proyectile)
+    {
+        yield return new WaitForSeconds(proyectileTimeAlive);
+        DespawnProyectile(proyectile);
+    }
+
+    private void DespawnProyectile(GameObject proyectile)
+    {
+        proyectile.SetActive(false);
+        proyectile.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        proyectile.transform.position = owner.transform.position;
+        pool.Enqueue(proyectile);
+    }
+
 }

9de1bf66e1e15b28e5ad1702d43c3cc5b2140de9
Will7461 <williamm@ucm.es>
2022-11-13 19:53:43 +0100

    Creditos

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+	[SerializeField] GameObject creditsPanel;
+
     public void SwitchScene(string name)
 	{
 		GameManager.Instance.SetScene(name);
+	}
+
+	public void ShowCredits()
+	{
+		creditsPanel.SetActive(true);
 	}
 
 	public void ExitGame()

2f8a5b5218e9c49b2990748b2a8763db4e1764aa
Will7461 <43720307+Will7461@users.noreply.github.com>
2022-11-13 01:35:08 +0100

    Llegada a metro funcional (#22)
    * SmoothCamera
    
    * Pickable Objects
    
    * TextMeshPro
    
    * Sistema de puntos
    
    * UIManager
    
    * Sistema de sonido + coin sound
    
    * Enemigos que disparan
    
    * Update Will Scene
    
    * Enemigo torreta
    
    * Scene Transition Fade
    
    * Maqueta nivel
    
    * Power Ups Modelos
    
    * Shooting Player
    
    * Sistema de loot random
    
    * Menu de pausa
    
    * Main menu
    
    * Llegada al metro

---
+++ Assets/EndPoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+	[SerializeField] string nextSceneName;
+	[SerializeField] int minPoints;
+	private void OnTriggerEnter(Collider other)
+	{
+		PlayerManager pM = other.GetComponent<PlayerManager>();
+		if (pM && GameManager.Instance.CurrentLevelManager.Points >= minPoints)
+			GameManager.Instance.SetScene(nextSceneName);
+	}
+}

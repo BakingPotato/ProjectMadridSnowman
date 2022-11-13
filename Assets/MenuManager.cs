ec33405565096cefc8faff64b3cba53956e850b4
Will7461 <43720307+Will7461@users.noreply.github.com>
2022-11-13 21:01:21 +0100

    Boton ajustes en Menu (#35)
    * SmoothCamera
    
    * Pickable Objects
    
    * TextMeshPro
    
    * Sistema de puntos
    
    * UIManager
    
    * Sistema de sonido + coin sound
    
    * Enemigos que disparan
    
    * Update Will Scene
    
    * Enemigo torreta
    
    * Scene Transition Fade
    
    * Maqueta nivel
    
    * Power Ups Modelos
    
    * Shooting Player
    
    * Sistema de loot random
    
    * Menu de pausa
    
    * Main menu
    
    * Llegada al metro
    
    * Pausa definitiva
    
    * Main menu definitivo
    
    * Update default-2021.dwlt
    
    * TripleShoot + ShootingSpeed
    
    * Update default-2021.dwlt
    
    * Main menu
    
    * Some changes
    
    * Barras de vida enemigos
    
    * GUI
    
    * SavePrefab
    
    * Merge prefabs enemies
    
    * Update Will.unity
    
    * Organización en LevelEngine
    
    * Update LevelEngine.unity
    
    * Ajust GUI
    
    * Creditos
    
    * Ajustes en Main Menu

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-	[SerializeField] GameObject creditsPanel;
+	[SerializeField] Slider musicSlider;
+	[SerializeField] Slider sfxSlider;
 
-    public void SwitchScene(string name)
+	private void Start()
+	{
+		musicSlider.value = AudioManager.Instance.MusicVolume;
+		sfxSlider.value = AudioManager.Instance.SFXVolume;
+	}
+	public void SwitchScene(string name)
 	{
 		GameManager.Instance.SetScene(name);
-	}
-
-	public void ShowCredits()
-	{
-		creditsPanel.SetActive(true);
 	}
 
 	public void ExitGame()
 	{
 		Application.Quit();
+	}
+	public void SetMusicVolume(float v)
+	{
+		AudioManager.Instance.MusicVolume = v;
+	}
+	public void SetSFXVolume(float v)
+	{
+		AudioManager.Instance.SFXVolume = v;
 	}
 }

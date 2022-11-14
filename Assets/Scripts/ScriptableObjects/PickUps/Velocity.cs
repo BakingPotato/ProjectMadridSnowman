94c4cb83559ef73b8eb34b7d091e4720b267e5d0
Pistoncito <victorglezrivera@hotmail.com>
2022-11-14 20:07:37 +0100

    Sonidos para curarse, powerups y snowBox al romperse

--- Assets/Scripts/ScriptableObjects/PickUps/Velocity.cs
+++ Assets/Scripts/ScriptableObjects/PickUps/Velocity.cs
@@ -6,9 +6,9 @@ public class Velocity : PickUpObject
 	public float amount;
 	public float time = -1;
 	public override void Apply(GameObject target)
-	{
-		target.GetComponent<PlayerManager>().increaseMovementSpeed(amount, time);
+    {
 
-		//AudioManager.Instance.PlaySFX("VelocityUp");
-	}
+        target.GetComponent<PlayerManager>().increaseMovementSpeed(amount, time);
+        AudioManager.Instance.PlaySFX3D("PowerUp", target.transform.position);
+    }
 }

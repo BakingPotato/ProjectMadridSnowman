0b451c0bd3b05e6233c32c665ad2c1ddbf17b1cc
BakingPotato <36265666+BakingPotato@users.noreply.github.com>
2022-11-14 01:42:10 +0100

    Demo2 acabada con las mejoras de Victor y Will + regalito (#42)
    * Los projectiles detectan los paraguas
    
    * Interacción balas-powerup-enemigos mejorada
    
    * Mejorados los prefabs enemigos
    
    * Test Level
    
    * Reajuste de nivel test
    
    * Cambios en la escena test
    
    * Cambiado prefab mainCamara para que sea como la de Will
    
    * Cambios para la demo
    
    * Update Patata
    
    * Spawner de PowerUps
    
    * Importados madroño, fuente, arboles, paraguas y pantalla de resultados
    
    * Cambios en mi escena
    
    * Añadido resultados al canvas, cambiadas velocidades del jugador y enemigos
    
    * Mejoras en la escena de patata
    
    * Cibeles, metro mejorado y muñeco torreta
    
    * Jamón, Alcala2, muros y muñecos de nieve
    
    * Testeo de bug de movimiento
    
    * Cambios en model engine
    
    * testing-level2
    
    * Segunda demo acabada
    
    Co-authored-by: Will7461 <williamm@ucm.es>

---
+++ Assets/Scripts/PowerUp/EnemySpawner.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<GameObject> ranged;
+    [SerializeField] List<GameObject> melee;
+
+    public enum EnemyType
+    {
+        Melee, Ranged
+    }
+
+    [System.Serializable]
+    public struct Enemy
+    {
+        public EnemyType type;
+        public float weight;
+    }
+
+    [SerializeField] protected Enemy[] enemyArray;
+
+    public bool isSpawning { get { return spawning != null; } }
+    Coroutine spawning = null;
+
+    [SerializeField] float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnRandomEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isSpawning)
+        {
+            //Paramos la corrutina por si entro uno antes y empezamos
+            spawning = StartCoroutine(spawnEnemy());
+        }
+    }
+
+    IEnumerator spawnEnemy()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SpawnRandomEnemy();
+        spawning = null;
+    }
+
+    private void SpawnRandomEnemy()
+    {
+        EnemyType randomType = GetRandomEnemyType();
+
+        switch (randomType)
+        {
+            case EnemyType.Ranged:
+                Instantiate(ranged[Random.Range(0, ranged.Count)], transform.position, Quaternion.identity);
+                break;
+            case EnemyType.Melee:
+                Instantiate(melee[Random.Range(0, melee.Count)], transform.position, Quaternion.identity);
+                break;
+        }
+    }
+
+    protected EnemyType GetRandomEnemyType()
+    {
+        float totalWeight = 0;
+        foreach (Enemy loot in enemyArray)
+        {
+            totalWeight += loot.weight;
+        }
+
+        float p = Random.Range(0, totalWeight);
+        float runningTotal = 0;
+
+        foreach (Enemy enemy in enemyArray.Reverse())
+        {
+            runningTotal += enemy.weight;
+            if (p < runningTotal) return enemy.type;
+        }
+
+        return EnemyType.Melee;
+    }
+}
--- Assets/Scripts/PowerUp/Spawner.cs
+++ Assets/Scripts/PowerUp/Spawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpSpawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject powerUp;
     [SerializeField] float waitTime;

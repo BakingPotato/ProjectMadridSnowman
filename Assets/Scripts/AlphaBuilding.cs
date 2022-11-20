84db2d24e4ca54ae329bfbcdc07eeb464a9059c7
Will7461 <43720307+Will7461@users.noreply.github.com>
2022-11-20 22:48:49 +0100

    Edificios semitransparentes (#69)
    * SmoothCamera
    
    * Pickable Objects
    
    * TextMeshPro
    
    * Sistema de puntos
    
    * UIManager
    
    * Sistema de sonido + coin sound
    
    * Enemigos que disparan
    
    * Update Will Scene
    
    * Enemigo torreta
    
    * Scene Transition Fade
    
    * Maqueta nivel
    
    * Power Ups Modelos
    
    * Shooting Player
    
    * Sistema de loot random
    
    * Menu de pausa
    
    * Main menu
    
    * Llegada al metro
    
    * Pausa definitiva
    
    * Main menu definitivo
    
    * Update default-2021.dwlt
    
    * TripleShoot + ShootingSpeed
    
    * Update default-2021.dwlt
    
    * Main menu
    
    * Some changes
    
    * Barras de vida enemigos
    
    * GUI
    
    * SavePrefab
    
    * Merge prefabs enemies
    
    * Update Will.unity
    
    * Organización en LevelEngine
    
    * Update LevelEngine.unity
    
    * Ajust GUI
    
    * Creditos
    
    * Ajustes en Main Menu
    
    * Update Will.unity
    
    * Bugfix: apuntar bien
    
    * Update Patata.unity
    
    * Update EditorUserSettings.asset
    
    * mensajes de info
    
    * Ajuste paraguas
    
    * feedback visual tiempo disparo jugador
    
    * Importados modelos
    
    * Puntos en €
    
    * Ajustes en creditos
    
    * Update NieveSnowBall.prefab
    
    * Cambios en LevelEngine Angulo camara
    
    * Mejora visual café
    
    * Loot colocado
    
    * Cursor y mensaje para pasar a la estacion
    
    * Ajustes altura item lvl1
    
    * Colores cajas
    
    * Ajustado enemigo torreta que gira
    
    * Update Level1.unity
    
    * Actualizada UI derrota y Cursor
    
    * Cantalla resultados definitiva
    
    * Far 100 camera
    
    * Ajuste Results
    
    * Creditos actualizados
    
    * Save MainMenu
    
    * Audios metidos
    
    * AudioManager mejorado
    
    * MainMenu1
    
    * Intro video
    
    * Update Canvas.prefab
    
    * Subidos audios
    
    * Updates
    
    * Warn
    
    * Update ProjectSettings.asset
    
    * Update GameOver
    
    * BugFixes
    
    * Alpha buildings
    
    * Update

---
+++ Assets/Scripts/AlphaBuilding.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class AlphaBuilding : MonoBehaviour
+{
+	bool _halfAlpha = false;
+	Material[] _materials;
+	float _alphaTime = 0.5f;
+
+	public bool HalfAlpha { get => _halfAlpha; set => _halfAlpha = value; }
+
+	private void Start()
+	{
+		_materials = GetComponent<Renderer>().materials;
+	}
+
+	void SetMatValues(Material mat)
+	{
+		mat.SetFloat("_Mode", 3);
+		mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+		mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+		mat.SetInt("_ZWrite", 0);
+		mat.DisableKeyword("_ALPHATEST_ON");
+		mat.EnableKeyword("_ALPHABLEND_ON");
+		mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+		mat.renderQueue = 3000;
+	}
+
+	void ResetMatValues(Material mat)
+	{
+		mat.SetFloat("_Mode", 0);
+		mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+		mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+		mat.SetInt("_ZWrite", 1);
+		mat.DisableKeyword("_ALPHATEST_ON");
+		mat.DisableKeyword("_ALPHABLEND_ON");
+		mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+		mat.renderQueue = -1;
+	}
+	public void MakeHalfAlpha()
+	{
+		if (HalfAlpha)
+		{
+			CancelInvoke("ResetAlpha");
+			Invoke("ResetAlpha", _alphaTime);
+			return;
+		}
+
+		HalfAlpha = true;
+
+		foreach (Material mat in _materials)
+		{
+			SetMatValues(mat);
+
+			Color alphaColor = mat.color;
+			alphaColor.a = 0.3f;
+			mat.color = alphaColor;
+		}
+
+		foreach (Transform child in transform)
+		{
+			Renderer r = child.GetComponent<Renderer>();
+			if (r)
+			{
+				SetMatValues(r.material);
+
+				Color alphaColor = r.material.color;
+				alphaColor.a = 0.3f;
+				r.material.color = alphaColor;
+			}
+		}
+
+		Invoke("ResetAlpha", _alphaTime);
+	}
+
+	void ResetAlpha()
+	{
+		HalfAlpha = false;
+
+		foreach (Material mat in _materials)
+		{
+			ResetMatValues(mat);
+
+			Color alphaColor = mat.color;
+			alphaColor.a = 1f;
+			mat.color = alphaColor;
+		}
+
+		foreach (Transform child in transform)
+		{
+			Renderer r = child.GetComponent<Renderer>();
+			if (r)
+			{
+				ResetMatValues(r.material);
+
+				Color alphaColor = r.material.color;
+				alphaColor.a = 1f;
+				r.material.color = alphaColor;
+			}
+		}
+	}
+}

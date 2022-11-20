9b709ac6267095c0b0c9825393efb2f9d6b790ab
Will7461 <williamm@ucm.es>
2022-11-20 22:48:12 +0100

    Update

--- Assets/Scripts/AlphaBuilding.cs
+++ Assets/Scripts/AlphaBuilding.cs
@@ -21,7 +21,7 @@ public class AlphaBuilding : MonoBehaviour
 		mat.SetFloat("_Mode", 3);
 		mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
 		mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-		//mat.SetInt("_ZWrite", 0);
+		mat.SetInt("_ZWrite", 0);
 		mat.DisableKeyword("_ALPHATEST_ON");
 		mat.EnableKeyword("_ALPHABLEND_ON");
 		mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");

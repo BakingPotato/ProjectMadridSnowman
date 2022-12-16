a39a391384308cbd5b56558eda235bea3005442d
BakingPotato <36265666+BakingPotato@users.noreply.github.com>
2022-12-16 18:56:36 +0100

    Añadidos cubos de narrativa y mejoras de optimización

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -12,6 +12,6 @@ public class EndPoint : MonoBehaviour
 		if (pM && GameManager.Instance.CurrentLevelManager.Points >= minPoints)
 			GameManager.Instance.ShowResults(nextSceneName);
 		else if(pM)
-			GameManager.Instance.CurrentLevelManager.UIManager.SendTextInfo("Necesitas como m�nimo " + minPoints / 100 + "," + (minPoints % 100).ToString("00") + " para pasar a la estaci�n.", 4.5f);
+			GameManager.Instance.CurrentLevelManager.UIManager.SendTextInfo("Necesito " + minPoints / 100 + "," + (minPoints % 100).ToString("00") + " euros para pasar a la estación.", 4.5f);
 	}
 }

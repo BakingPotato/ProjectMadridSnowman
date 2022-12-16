763fcb36cafa81b8e590b5e1777b419c29915454
thekeytothekey <118301595+thekeytothekey@users.noreply.github.com>
2022-12-16 20:22:43 +0100

    Música
    Actualizada música RiderX y añadida he implementada música de resultados

--- Assets/LVLvsGOMusic.cs
+++ Assets/LVLvsGOMusic.cs
@@ -7,6 +7,8 @@ public class LVLvsGOMusic : MonoBehaviour
     [SerializeField] GameObject _lvlMusic;
     [SerializeField] GameObject _altMusic1;
     [SerializeField] GameObject _altMusic2;
+    [SerializeField] GameObject _altMusic3;
+
 
     void Start()
     {
@@ -16,7 +18,7 @@ public class LVLvsGOMusic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_altMusic1.activeSelf | _altMusic2.activeSelf)
+        if (_altMusic1.activeSelf | _altMusic2.activeSelf | _altMusic3.activeSelf)
         {
             _lvlMusic.SetActive(false);
         }
